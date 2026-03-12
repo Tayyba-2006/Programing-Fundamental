1068b923d324f522dd2cda381b2ddb43bd850786
Tayyba-2006 <tayybahasnain2006@gmail.com>
2026-03-12 21:43:13 +0500

    uploaded cpp file

---
+++ Lab/Week4/checkcharacter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "Enter any character in lower case: ";
+    cin >> c;
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
+    {cout<<"It is a vowel.\n";}
+    else if(c >= '0' && c <= '9' )
+    {cout<<"It is a number.\n";}
+    else if((c >= 'a' &&  c <= 'z')) 
+    {cout<<"It is a consonant."<<endl;}
+    else
+    {cout<<"This is a special character or symbol!";
+    }
+    return 0;
+}
