3e667658b546ae18c0b2ee7b1b2f64783881e048
Tayyba-2006 <tayybahasnain2006@gmail.com>
2026-03-13 10:30:24 +0500

    problem solved

--- Lab/Week4/flowershop.cpp
+++ Lab/Week4/flowershop.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    float redrosenum, whiterosenum, tulipsnum, original_price, dis, priceafterdis;
+    cout<<"Enter the number of red roses: ";
+    cin>>redrosenum;
+    cout<<"Enter the number of white roses: ";
+    cin>>whiterosenum;
+    cout<<"Enter the number of tulips: ";
+    cin>>tulipsnum;
+    original_price= (redrosenum*2.00)+(whiterosenum*4.10)+(tulipsnum*2.50);
+    if(original_price>200)
+    {dis=original_price*20/100;
+     priceafterdis=original_price-dis; 
+     cout<<"Original price: $"<<original_price;
+     cout<<"Price after discount: $"<<priceafterdis;
+    }
+    return 0;
 }
