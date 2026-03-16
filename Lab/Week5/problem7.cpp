536e39e66e0d939e6e06542ff05d264d011a9220
Tayyba-2006 <tayybahasnain2006@gmail.com>
2026-03-16 14:49:00 +0500

    problem solves

--- Lab/Week5/problem7.cpp
+++ Lab/Week5/problem7.cpp
@@ -5,7 +5,14 @@ main()
     int n1 = 0, n2 = 1, n, next;
     cout<<"Enter the length of the Fibonacci Series: ";
     cin>>n;
+    if(n>=2)
+    {
     cout<<n1<<" , "<<n2;
+    }
+    else if(n==1)
+    {
+    cout<<n1;   
+    }
     for(int x=1;x<n-1;x++)
     {
         next=n1+n2;
